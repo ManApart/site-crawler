baf4b7c741c5d22fdd034c3a1ba58e3872d3f98b
ManApart <raustinkucera@gmail.com>
2020-02-11 20:50:05 -0500

    create index page

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,19 +3,33 @@ import java.net.URL
 import java.nio.charset.StandardCharsets
 import java.util.*
 
-
+const val SITE_NAME = "Philosophy of Unix"
 const val SITE_URL = "http://www.catb.org/esr/writings/taoup/html/index.html"
-const val MAX_DEPTH = 5
+const val MAX_DEPTH = 1000
+
+const val HEADER = "<html>\n" +
+        "<body>\n" +
+        "  <h1>Table of Contents</h1>\n"
+
+const val FOOTER = "</body>\n" +
+        "\n" +
+        "</html>"
+
 
 fun main(args: Array<String>) {
     crawl(SITE_URL, NextLinkFetcher())
 }
 
-fun crawl(url: String, fetcher: PageFetcher, depth: Int = MAX_DEPTH) {
+fun crawl(url: String, fetcher: PageFetcher, depth: Int = 0, pageList: MutableList<String> = mutableListOf(), previousData: String = "") {
     val data = fetchData(url)
-    saveData(url, data)
-    if (fetcher.hasNext(data) && depth > 0) {
-        crawl(fetcher.getNextUrl(data), fetcher, depth - 1)
+    val fileName = "$depth${url.substring(url.lastIndexOf("/") + 1)}"
+    val pathName = "./download/$fileName"
+    saveData(pathName, data)
+    pageList.add(fileName)
+    if (fetcher.hasNext(data) && depth < MAX_DEPTH && data != previousData) {
+        crawl(fetcher.getNextUrl(data), fetcher, depth + 1, pageList, data)
+    } else {
+        createIndexPage(pageList)
     }
 }
 
@@ -29,10 +43,20 @@ private fun fetchData(url: String): String {
     }
 }
 
-private fun saveData(url: String, data: String) {
-    val fileName = "./download/" + url.substring(url.lastIndexOf("/"))
+private fun saveData(fileName: String, data: String) {
     File(fileName).printWriter().use { out ->
         out.println(data)
     }
+}
+
+private fun createIndexPage(pageList: List<String>) {
+    var indexData = HEADER
+    pageList.forEach {
+        indexData += "<a href=\"$it\">$it</a><br />\n"
+    }
+    indexData += FOOTER
+
+    println(indexData)
+    saveData("./download/$SITE_NAME.html", indexData)
 }
 
