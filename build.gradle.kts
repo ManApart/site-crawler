f15bb64dbe77f02410b0ebba615f2371433707e6
ManApart <r@austinkucera.com>
2023-09-04 08:27:19 -0400

    bump jsoup

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
-    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("org.jsoup:jsoup:1.16.1")
     testImplementation("junit", "junit", "4.12")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
