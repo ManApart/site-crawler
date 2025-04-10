593425b81f2aae474330202ef7d954e81a62f8cf
ManApart <r@austinkucera.com>
2025-04-10 08:18:46 -0400

    asset info can crawl to more infos

--- src/main/kotlin/assetDownloader/AssetDownloader.kt
+++ src/main/kotlin/assetDownloader/AssetDownloader.kt
@@ -1,13 +1,11 @@
 package assetDownloader
 
-import assetDownloader.downloaders.ArtStationDownloader
-import assetDownloader.downloaders.FiftyNineParksDownloader
+import assetDownloader.downloaders.ImperialLibraryDownloader
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.net.URL
-import java.net.URLConnection
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files.copy
 import java.nio.file.StandardCopyOption
@@ -15,27 +13,29 @@ import java.util.*
 import java.util.Collections.emptyList
 
 const val MAX_DEPTH = 1000
-const val CHUNK_SIZE = 3
+const val CHUNK_SIZE = 10
 const val DOWNLOAD_FIRST_ONLY = false
 const val CRAWL_LOCAL = false
+const val REDOWNLOAD_EXISTING = false
 
-fun main() {
 //    val fetcher = XboxScreenShotDownloader("iceburg-33308")
 //    val fetcher = XboxClipDownloader("iceburg33308")
 //    val fetcher = ESOWallpaperDownloader()
 //    val fetcher = WikiArtDownloader("https://www.wikiart.org/en/norman-rockwell/all-works/text-list")
-    val fetcher = FiftyNineParksDownloader()
+val fetcher = ImperialLibraryDownloader()
+
+//    val fetcher = FiftyNineParksDownloader()
 //    val fetcher = ArtStationDownloader("https://www.artstation.com/nicatorshields", listOf("https://cdna.artstation.com/p/assets/images/", "https://cdnb.artstation.com/p/assets/images/"))
 //    val fetcher = PexelsDownloader("forest", 11, 20, 200, "H2jk9uKnhRmL6WPwh89zBezWvr")
 //    val fetcher = PrintableBrickDownloader(60, 40)
 //    val fetcher = StarfieldWikiDownloader("https://starfieldwiki.net/wiki/Category:Starfield-Skill_Images")
+fun main() {
     crawlAndDownload(fetcher, fetcher.baseUrl(), fetcher.getHeaders())
 }
 
 private fun crawlAndDownload(fetcher: AssetPageFetcher, url: String, headers: Map<String, String>, depth: Int = 0) {
     val (data, assetInfos) = if (CRAWL_LOCAL) crawlLocal(fetcher) else crawl(fetcher, url, headers)
     println("Found ${assetInfos.size} assets.")
-
 
     if (DOWNLOAD_FIRST_ONLY) {
         download(assetInfos.first())
@@ -77,9 +77,7 @@ private fun crawlLocal(fetcher: AssetPageFetcher, useHtml: Boolean = true): Pair
 }
 
 fun fetchData(url: String, headers: Map<String, String>): String {
-    val connection: URLConnection = URL(url).openConnection()
-    with(connection) {
-        //fake we're a browser for https
+    with(URL(url).openConnection()) {
         setRequestProperty(
             "User-Agent",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
@@ -88,13 +86,13 @@ fun fetchData(url: String, headers: Map<String, String>): String {
             setRequestProperty(key, value)
         }
         connect()
-    }
-    Scanner(
-        connection.getInputStream(),
-        StandardCharsets.UTF_8.toString()
-    ).use { scanner ->
-        scanner.useDelimiter("\\A")
-        return if (scanner.hasNext()) scanner.next() else ""
+        Scanner(
+            getInputStream(),
+            StandardCharsets.UTF_8.toString()
+        ).use { scanner ->
+            scanner.useDelimiter("\\A")
+            return if (scanner.hasNext()) scanner.next() else ""
+        }
     }
 
 }
@@ -112,26 +110,35 @@ fun downloadChunk(infos: List<AssetInfo>, i: Int, totalChunks: Int) {
         }.awaitAll()
     }
 }
-//TODO - if additional infos function fetch them from page
+
 private fun download(info: AssetInfo) {
-    val connection: URLConnection = URL(info.url).openConnection()
-    try {
-        //fake we're a browser for https
-        connection.setRequestProperty(
-            "User-Agent",
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11"
-        )
-        connection.connect()
-    } catch (e: Exception) {
-        println("Failed to connect to ${info.url}")
-    }
-
     val file = File(info.fileName)
-    try {
-        copy(connection.getInputStream(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-    } catch (ex: Exception) {
-        println("Failed to download ${info.fileName} from ${info.url}")
-        println(ex)
-    }
+    val download = !file.exists() || REDOWNLOAD_EXISTING
+    if (info.additionalInfos != null) {
+        val data = if (download) fetchData(info.url, fetcher.getHeaders()) else file.readText()
+        if (!file.exists()) File(info.fileName).writeText(data)
+        info.additionalInfos.invoke(data).forEach { download(it) }
+    } else if (download) downloadToFile(info)
+}
 
+private fun downloadToFile(info: AssetInfo) {
+    with(URL(info.url).openConnection()) {
+        try {
+            setRequestProperty(
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11"
+            )
+            connect()
+        } catch (e: Exception) {
+            println("Failed to connect to ${info.url}")
+        }
+
+        try {
+            val file = File(info.fileName)
+            copy(getInputStream(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (ex: Exception) {
+            println("Failed to download ${info.fileName} from ${info.url}")
+            println(ex)
+        }
+    }
 }
--- src/main/kotlin/assetDownloader/downloaders/ImperialLibraryDownloader.kt
+++ src/main/kotlin/assetDownloader/downloaders/ImperialLibraryDownloader.kt
@@ -20,18 +20,24 @@ class ImperialLibraryDownloader() : AssetPageFetcher {
     override fun getAssetInfos(url: String, pageData: String): List<AssetInfo> {
         return Jsoup.parse(pageData)
             .select("a.title")
-            .map { it.attr("src") }
             .map {
                 //TODO - parse contents page
                 //TODO - pass additional infos for chapter sections
                 //  - filename group by book
-                AssetInfo(it.replace("smaller_square", "large"), "./download/${it.cleanTitle()}")
+                val title = it.text().cleanTitle()
+                AssetInfo(it.attr("href"), "./download/${title}.html") { data ->
+                    Jsoup.parse(data)
+                        .select("h5:contains(Table)").first()?.siblingElements()?.select("ul")
+                        ?.select("a")?.map { chapter ->
+                            val chTitle = chapter.text().cleanTitle()
+                            AssetInfo(chapter.attr("href"), "./download/${title}-$chTitle.html")
+                        } ?: emptyList()
+                }
             }
     }
 
     private fun String.cleanTitle(): String {
-        val end = if (indexOf("?") != -1) indexOf("?") else length
-        return substring(lastIndexOf("/") + 1, end)
+        return replace(" ", "_")
     }
 
 }
