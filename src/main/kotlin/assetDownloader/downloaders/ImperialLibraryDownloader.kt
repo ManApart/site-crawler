ff2ac9f4d3a730d88eacff4867577b2c41cea7a7
ManApart <r@austinkucera.com>
2025-06-06 09:19:13 -0400

    capture linked books

--- src/main/kotlin/assetDownloader/downloaders/ImperialLibraryDownloader.kt
+++ src/main/kotlin/assetDownloader/downloaders/ImperialLibraryDownloader.kt
@@ -5,7 +5,7 @@ import assetDownloader.AssetPageFetcher
 import org.jsoup.Jsoup
 
 //https://www.imperial-library.info/content/pocket-guide-empire-and-its-environs-first-edition#4cd0e52a-1a9a-4308-87e9-ae9d8af022be-link
-class ImperialLibraryDownloader() : AssetPageFetcher {
+class ImperialLibraryDownloader : AssetPageFetcher {
 
     override fun baseUrl() = "https://www.imperial-library.info/game-books/all-elder-scrolls-books"
 
@@ -19,17 +19,17 @@ class ImperialLibraryDownloader() : AssetPageFetcher {
 
     override fun getAssetInfos(url: String, pageData: String): List<AssetInfo> {
         val parsed = Jsoup.parse(pageData)
-        return (parsed.select("#menu-item-236").select("ul.sub-menu").select("a") +
-                parsed.select("a.title"))
+        return (parsed.select("#menu-item-236").select("ul.sub-menu").select("a")
+                + parsed.select("a.title"))
             .map {
                 val title = it.text().cleanTitle()
                 AssetInfo(it.attr("href"), "./download/${title}.html") { data ->
                     Jsoup.parse(data)
-                        .select("h5:contains(Table)").first()?.siblingElements()?.select("ul")
-                        ?.select("a")?.mapIndexed { i, chapter ->
+                        .select("#main").select("ul")
+                        .select("a").mapIndexed { i, chapter ->
                             val chUrl = chapter.attr("href").let { u -> if (u.startsWith("/")) "https://www.imperial-library.info$u" else u }
                             AssetInfo(chUrl, "./download/${title}-$i.html")
-                        } ?: emptyList()
+                        }
                 }
             }
     }
